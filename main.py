df29a3a7e11ea404701515e1eb6f6603f862fff2
Martin Brunclík <trenclikb1@gmail.com>
2026-03-18 08:32:11 +0100

    funkční verze s komentáři

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import batterylib
+import psutil # importování knihovny pro získání informací baterie
 
 while True:
-    baterie = batterylib.get_percentage()
-    if baterie == 67:
+    battery = psutil.sensors_battery() # vytvoření proměnné držící informace baterie
+    # musí být uvnitř smyčky, jinak se neobnoví hodnota
+    percent = int(battery.percent) # procenta baterie zaokrouhlené na celé číslo
+    if percent == 67: # pokud se procento baterie rovná 67 vypíše "six seven" a opustí smyčku
         print("six seven")
         break
-    elif baterie == 69:
+    elif percent == 69:
         print("nice")
-        continue
+        break
     else:
-        print("bruh")
+        continue
